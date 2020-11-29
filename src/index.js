3419242a9dde4af43ac57d9e571e23128169ebae
Дима Олейник <dimaoliynyk@MacBook-Air-Dima.local>
2020-11-29 14:32:15 +0200

    fx done

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import css from "./css/style.css";
+
+const countDownDate = new Date("08.01.2023 18:12:43");
+console.log(countDownDate);
+const timer = setInterval(function() {
+
+  const now = new Date();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  const daysL = document.getElementById('days').textContent = days;
+  const hoursL = document.getElementById('hours').textContent = hours;
+  const minutesL = document.getElementById('mins').textContent = minutes;
+  const secondsL = document.getElementById('secs').textContent = seconds;
+}, 1000);
